6cbc90c353a12265fc36856c4d0e0aee787602ff
sttk <sttk.xslet@gmail.com>
2021-12-19 17:05:38 +0900

    chore!: Normalize repository, dropping node <10.13 support

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function DefaultRegistry() {
   this._tasks = {};
 }
 
+/* eslint no-unused-vars: 0 */
 DefaultRegistry.prototype.init = function init(taker) {};
 
 DefaultRegistry.prototype.get = function get(name) {
